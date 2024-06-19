c4c704abe8d2a758faad9f9fb968d3d3b7cd592d
David Couvin <david.couvin@googlemail.com>
2024-06-19 16:51:11 -0400

    Update machine_learning.py

--- Python3_ML/machine_learning.py
+++ Python3_ML/machine_learning.py
@@ -28,8 +28,8 @@ parser.add_argument("-d", "--delimiter", metavar="delimiter",
                     help="choose your CSV files's delimiter (ex: ',', ';', '\t', ...)", type=str)
 parser.add_argument("-r", "--remove", metavar="remove",
                     help="remove CSV files's columns for prediction (ex: 'ID','Type','Country',...)", type=str)
-parser.add_argument("-c", "--class", metavar="class",
-                    help="Class used for prediction (ex: 'Type')", type=str)
+parser.add_argument("-l", "--label", metavar="label",
+                    help="Class or label used for prediction (ex: 'Type')", type=str)
 parser.add_argument("-e", "--estimators", metavar="estimators ", help="choose a value (10 by default)", default=10, type=int)
 parser.add_argument("-k", "--kneighbors", metavar="kneighbors ", help="choose a value (3 by default)", default=3, type=int)
 parser.add_argument("-o", "--output", help="Directs the output to a name of your choice", type=str)
@@ -41,12 +41,13 @@ model_option = args.model
 output = args.output
 delim = args.delimiter
 remove = args.remove
-cl = args.class
+list = list(remove.split(","))
+cl = args.label
 model_estimator = args.estimators
 model_kneighbors = args.kneighbors
 
-tb_data = pd.read_csv(in_file, delimiter=delim)
-
+tb_data = pd.read_csv(in_file, delimiter=delim, engine='python')
+tb_data = tb_data.dropna()
 
 # tb_data = pd.read_csv('tb_file.csv')
 X = tb_data.drop(columns=[remove]) #Type = Lineage ; Country = Family
@@ -175,7 +176,7 @@ with report as f:
         # X, y = load_iris(return_X_y=True)
         #X = tb_data.drop(columns=['ID', 'Lineage', 'Family'])
         #y = tb_data['Family']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
         gnb = GaussianNB()
         y_pred = gnb.fit(X_train, y_train).predict(X_test)
         score_nb = accuracy_score(y_test, y_pred)
@@ -328,103 +329,58 @@ with report as f:
         debut_dt = time.perf_counter()
         model_option = DecisionTreeClassifier()
         model_option.fit(X_train, y_train)
-        tree.export_graphviz(model_option, out_file='graph.dot',
-                             feature_names=['var1', 'var2', 'var3', 'var4', 'var5', 'var6', 'var7', 'var8', 'var9',
-                                            'var10',
-                                            'var11', 'var12', 'var13', 'var14', 'var15', 'var16', 'var17', 'var18',
-                                            'var19',
-                                            'var20',
-                                            'var21', 'var22', 'var23', 'var24', 'var25', 'var26', 'var27', 'var28',
-                                            'var29',
-                                            'var30',
-                                            'var31', 'var32', 'var33', 'var34', 'var35', 'var36', 'var37', 'var38',
-                                            'var39',
-                                            'var40',
-                                            'var41', 'var42', 'var43'],
-                             class_names=sorted(y.unique()),
-                             label='all',
-                             rounded=True,
-                             filled=True)
         predictions = model_option.predict(X_test)
-        prediction2 = model_option.predict(
-           # [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-           #   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
-           #  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-           #   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1],
-           #  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0,
-           #   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]])
-           [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 7, 1],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1],
-             [7, 7, 3, 7, 7, 7, 0, 3, 1, 1, 1, 0, 0, 0, 1]])
-
         score_dt = accuracy_score(y_test, predictions)
         fin_dt = time.perf_counter()
         print("Prediction of X_test: ", predictions)
-        print("Prediction of 2 spoligo: ", prediction2)
+        #print("Prediction of 2 spoligo: ", prediction2)
         print("dt score :")
         print(score_dt)
-        print(f" L'option NearestNeighbors {fin_dt - debut_dt:0.4f} secondes")
+        print(f" L'option DT {fin_dt - debut_dt:0.4f} secondes")
 
         # dt
         # svm
         # X = [[0, 0], [1, 1]]
         # y = [0, 1]
         debut_svm = time.perf_counter()
-        X = tb_data.drop(columns=['ID', 'Type', 'Country'])
-        y = tb_data['Type']
+        #X = tb_data.drop(columns=['ID', 'Type', 'Country'])
+        #y = tb_data['Type']
         clf = svm.SVC()
         clf.fit(X, y)
         # SVC()
-
         predictions = clf.predict(X_test)
-        svm_prediction = clf.predict(
-            [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1],
-             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]])
-
         score_svm = accuracy_score(y_test, predictions)
         fin_svm = time.perf_counter()
-        print("Prediction of 2 spoligo with svm: ", svm_prediction)
+        #print("Prediction of 2 spoligo with svm: ", svm_prediction)
         print("svm score :")
         print(score_svm)
-        print(f" L'option svm {fin_svm - debut_svm:0.4f} secondes")
+        print(f" L'option SVM {fin_svm - debut_svm:0.4f} secondes")
         # svm
 
         # nn
         # X = [[0, 0], [1, 1]]
         # y = [0, 1]
         debut_nn = time.perf_counter()
-        X = tb_data.drop(columns=['ID', 'Type', 'Country'])
-        y = tb_data['Type']
+        #X = tb_data.drop(columns=['ID', 'Type', 'Country'])
+        #y = tb_data['Type']
         clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
         clf.fit(X, y)
         # MLPClassifier(alpha=1e-05, hidden_layer_sizes=(5, 2), random_state=1,solver='lbfgs')
         predictions = clf.predict(X_test)
-        nn_prediction = clf.predict(
-            [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1],
-             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]])
-
         score_nn = accuracy_score(y_test, predictions)
         fin_nn = time.perf_counter()
-        print("Prediction of 2 spoligo with nn: ", nn_prediction)
+        #print("Prediction of 2 spoligo with nn: ", nn_prediction)
         print("nn score :")
         print(score_nn)
-        print(f" L'option nn {fin_nn - debut_nn:0.4f} secondes")
+        print(f" L'option NN {fin_nn - debut_nn:0.4f} secondes")
         # nn
 
         # nb
         # X, y = load_iris(return_X_y=True)
         debut_nb = time.perf_counter()
-        X = tb_data.drop(columns=['ID', 'Type', 'Country'])
-        y = tb_data['Type']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+        #X = tb_data.drop(columns=['ID', 'Type', 'Country'])
+        #y = tb_data['Type']
+        #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
         gnb = GaussianNB()
         y_pred = gnb.fit(X_train, y_train).predict(X_test)
         score_nb = accuracy_score(y_test, y_pred)
@@ -446,10 +402,10 @@ with report as f:
         clf.fit(X, y)
         # SVC()
         predictions = clf.predict(X_test)
-        rf_prediction = clf.predict(tab)
+        #rf_prediction = clf.predict(tab)
         score_rf = accuracy_score(y_test, predictions)
         fin_rf = time.perf_counter()
-        print("Prediction of 2 spoligo with rf: ", rf_prediction)
+        #print("Prediction of 2 spoligo with rf: ", rf_prediction)
         print(score_rf)
 
         # rf
@@ -460,10 +416,10 @@ with report as f:
         clf = ExtraTreesClassifier(n_estimators=model_estimator)
         clf.fit(X, y)
         predictions = clf.predict(X_test)
-        rf_prediction = clf.predict(tab)
+        #rf_prediction = clf.predict(tab)
         score_et = accuracy_score(y_test, predictions)
         fin_et = time.perf_counter()
-        print("Prediction of 2 spoligo with rf: ", rf_prediction)
+        #print("Prediction of 2 spoligo with rf: ", rf_prediction)
         print(score_et)
         # scores = cross_val_score(clf, X, y, cv=5)
         # print(scores.mean())
@@ -474,10 +430,10 @@ with report as f:
         clf = AdaBoostClassifier(n_estimators=model_estimator)
         clf.fit(X, y)
         predictions = clf.predict(X_test)
-        ab_prediction = clf.predict(tab)
+        #ab_prediction = clf.predict(tab)
         score_ab = accuracy_score(y_test, predictions)
         fin_ab = time.perf_counter()
-        print("Prediction of 2 spoligo with ab: ", ab_prediction)
+        #print("Prediction of 2 spoligo with ab: ", ab_prediction)
         print(score_ab)
         print(f" L'option ab {fin_ab - debut_ab:0.4f} secondes")
         # ab
@@ -487,12 +443,12 @@ with report as f:
         clf = KNeighborsClassifier(n_neighbors=model_kneighbors)
         clf.fit(X, y)
         predictions = clf.predict(X_test)
-        knn_prediction = clf.predict(tab)
+        #knn_prediction = clf.predict(tab)
         score_knn = accuracy_score(y_test, predictions)
         fin_knn = time.perf_counter()
-        print("Prediction of 2 spoligo with knn: ", knn_prediction)
+        #print("Prediction of 2 spoligo with knn: ", knn_prediction)
         print(score_knn)
-        print(f" L'option Nayve Bayes {fin_knn - debut_knn:0.4f} secondes")
+        print(f" L'option KNN {fin_knn - debut_knn:0.4f} secondes")
         # knn
         df = pd.DataFrame([('Score', score_dt, score_rf, score_et, score_ab, score_svm, score_nn, score_nb, score_knn),
                            ('Temp(s)', f'{fin_dt - debut_dt:0.4}', f'{fin_rf - debut_rf:0.4}', f'{fin_et - debut_et:0.4}', f'{fin_ab - debut_ab:0.4}', f'{fin_svm - debut_svm: 0.4}',
@@ -503,7 +459,7 @@ with report as f:
         df.to_csv(output, index=False)
     # all_options
 fin = time.perf_counter()
-print(f" Le script à tourné durant {fin - debut:0.4f} secondes")
+print(f" The total running time is: {fin - debut:0.4f} secondes")
 
     # if args.all == True:
     #     f.write("Metrics/Models;DT;SVM;NN;NB;AB;KNN\n")
